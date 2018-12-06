bcceab1698d83ef7e256ac9c323376be2139608b
liuyh73 <15989067460@163.com>
2018-12-06 22:56:04 +0800

    fix bugs

--- svc/main.go
+++ svc/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/liuyh73/md5"
 )
 
 func main() {
-	fmt.Println(md5.Encrypt("message digest"))
+	fmt.Println(md5.Encrypt("The quick brown fox jumps over the lazy dog")) //9e107d9d372bb6826bd81d3542a419d6
+	fmt.Println(md5.Encrypt("The quick brown fox jumps over the lazy cog")) // 1055d3e698d289f2af8663725127bd4b
+	fmt.Println(md5.Encrypt(""))                                            // d41d8cd98f00b204e9800998ecf8427e
 }
